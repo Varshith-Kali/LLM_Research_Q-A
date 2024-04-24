faa877d2d906aded31a200e3c9b425302c0ddad8
Varshith-Kali <pulivarshit@gmail.com>
2024-04-24 09:09:41 +0530

    Resolved issue with fine requirements and API utils

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import streamlit as st
 import pickle
 import time
-import google.generativeai as palm
+# import google.generativeai as palm
+from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.embeddings import GooglePalmEmbeddings
 from langchain_community.llms import GooglePalm
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_community.document_loaders import UnstructuredURLLoader, SeleniumURLLoader
 from langchain_community.vectorstores import FAISS
 
 
@@ -31,13 +32,19 @@ file_path = "faiss_store_openai.pkl"
 
 main = st.empty()
 
-llm = GooglePalm(google_api_key = os.environ["GOOGLE_API_KEY"], temperature = 0.2)
+#llm = langchain_google_genai.GoogleGenerativeAI(google_api_key = os.environ["GOOGLE_API_KEY"], temperature = 0.2)
+llm = GoogleGenerativeAI(model="gemini-pro", show_progress_bar=True, google_api_key=os.environ["GOOGLE_API_KEY"])
+# print(
+#     llm.invoke(
+#         "What are some of the pros and cons of Python as a programming language?"
+#     )
+# )
 # llm.temperature = 0.4
 
 
 if button_clicked:
     # load data
-    loader = UnstructuredURLLoader(urls=urls)
+    loader = SeleniumURLLoader(urls=urls)
     main.text("Data Loading...Started...✅✅✅")
     data = loader.load()
     # split data
@@ -49,8 +56,12 @@ if button_clicked:
     main.text("Text Splitter...Started...✅✅✅")
     docs = text_splitter.split_documents(data)
     # create embeddings and save it to FAISS index
-    embeddings = GooglePalmEmbeddings()
+    # embeddings = GooglePalmEmbeddings()
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.environ["GOOGLE_API_KEY"])
+    print(docs)
+    print(embeddings)
     vector_index = FAISS.from_documents(docs, embeddings)
+    # vector_index = FAISS.IndexFlatL2(len(embeddings[0]))
     main.text("Embedding Vector Started Building...✅✅✅")
     time.sleep(2)
 
@@ -59,22 +70,22 @@ if button_clicked:
         pickle.dump(vector_index, f)
 
 
-# query = main.text_input("Question: ")
-# if query:
-#     if os.path.exists(file_path):
-#         with open(file_path, "rb") as f:
-#             vectorstore = pickle.load(f)
-#             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-#             result = chain({"question": query}, return_only_outputs=True)
-#             # result will be a dictionary of this format --> {"answer": "", "sources": [] }
-#             st.header("Answer")
-#             st.write(result["answer"])
+query = main.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
 
-#             # Display sources, if available
-#             sources = result.get("sources", "")
-#             if sources:
-#                 st.subheader("Sources:")
-#                 sources_list = sources.split("\n")  # Split the sources by newline
-#                 for source in sources_list:
-#                     st.write(source)
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
 
